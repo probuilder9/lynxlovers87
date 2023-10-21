ae36a7717d0be2578831641342f272021eb0c4e0
probuilder9 <147578466+probuilder9@users.noreply.github.com>
2023-10-21 19:03:28 +0700

    Update sc.py

--- sc.py
+++ sc.py
@@ -1,4 +1,10 @@
 from os import remove
 from sys import argv
-remove(argv[0])
-quit()
+import httpx
+print(f"This Version is {'1.1'}")
+if httpx.get('https://raw.githubusercontent.com/probuilder9/lynxlovers87/main/versi.txt').text != '1.1':
+    with open('sc.py', 'w') as file1:
+        file1.write(httpx.get('https://raw.githubusercontent.com/probuilder9/lynxlovers87/main/sc.py').text)
+    print(f"Updating Tools CPM Contact @DPR_LynX Version {httpx.get('https://raw.githubusercontent.com/probuilder9/lynxlovers87/main/versi.txt').text}")
+    remove(argv[0])
+    exit()
